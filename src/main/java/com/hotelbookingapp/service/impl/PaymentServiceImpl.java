b4556a50ae7dc92a8091184884a90a4f5907d9ae
ShivcharanJalendra1908 <132427922+ShivcharanJalendra1908@users.noreply.github.com>
2024-08-22 01:03:35 +0530

    Update PaymentServiceImpl.java

--- src/main/java/com/hotelbookingapp/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/hotelbookingapp/service/impl/PaymentServiceImpl.java
@@ -27,7 +27,7 @@ public class PaymentServiceImpl implements PaymentService {
                 .totalPrice(bookingInitiationDTO.getTotalPrice())
                 .paymentStatus(PaymentStatus.COMPLETED) // Assuming the payment is completed
                 .paymentMethod(PaymentMethod.CREDIT_CARD) // Default to CREDIT_CARD
-                .currency(Currency.RS) // Default to USD
+                .currency(Currency.RS) // Default to Indian Rs.
                 .build();
 
         Payment savedPayment = paymentRepository.save(payment);
